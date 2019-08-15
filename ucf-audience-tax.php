aa49734182f501f85405572285e3c9b5868cf943
Jim Barnes <jimbarnesdeveloper@gmail.com>
2019-08-15 09:49:21 -0400

    Updated template placeholders

---
+++ ucf-audience-tax.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: UCF Audience Taxonomy
+Description: Provides the Audience taxonomy for WordPress.
+Version: 1.0.0
+Author: UCF Web Communications
+License: GPL3
+GitHub Plugin URI: UCF/UCF-Audience-Tax-Plugin
+*/
+
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
